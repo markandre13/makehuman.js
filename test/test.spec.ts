86c4f1b8b103ac095f07f1b94b7dd7c1d5cc8d7c
Mark-André Hopf <mhopf@mark13.org>
2020-09-13 12:39:38 +0200

    read and dump a text file

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,20 @@
 import { expect } from "chai"
+import { URL } from "url"
+import * as fs from "fs"
+import * as readline from "readline"
 
 describe("test A", ()=> {
-    it("flies!", ()=> {
-        expect(1).to.equal(1)
+    it("flies!", async ()=> {
+        // let url = new URL("file://./data/3dobjs/base.obj")
+        let url = "data/3dobjs/base.obj"
+        
+        let stream = fs.createReadStream(url)
+        let reader = readline.createInterface({
+             input: stream,
+             crlfDelay: Infinity
+        })
+        for await (let line of reader) {
+            console.log(line)
+        }
     })
 })
