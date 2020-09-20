9732c465362bf666e6ee2a524f3a35f7e071d464
Mark-André Hopf <mhopf@mark13.org>
2020-09-20 11:08:52 +0200

    refactor

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,7 +1,5 @@
-// import { ipcRenderer } from 'electron'
 import { mat4, vec3 } from 'gl-matrix'
 import { WavefrontObj } from "./fileformats/WavefrontObj"
-// import * as readline from 'readline'
 
 declare global {
     interface Window {
@@ -16,7 +14,7 @@ let cubeRotation = 0.0
 
 // this works via webserver and electron!
 // TODO: have a look into Fetch API (https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
-// which provides streams
+// which provides a streaming api to save memory
 function get(url: string): Promise<string> {
 	return new Promise((succeed, fail) => {
 		const req = new XMLHttpRequest();
@@ -36,7 +34,6 @@ function get(url: string): Promise<string> {
 }
 
 async function main() {
-
     const glframe = document.createElement("div") // as HTMLCanvasElement
     glframe.style.position = "absolute"
     glframe.style.left = "0"
@@ -45,53 +42,14 @@ async function main() {
     glframe.style.bottom = "0"
     glframe.style.overflow = "hidden"
 
-    // document.body.style.overflow = "hidden"
-
     const canvas = document.createElement("canvas")
     canvas.style.width = "100vw"
-    canvas.style.height = "100vw"
+    canvas.style.height = "100vh"
     canvas.style.display = "block"
-
-    window.onresize = (ev: UIEvent)=> {
-        console.log(ev)
-        console.log(canvas.clientWidth)
-        console.log(canvas.clientHeight)
-        console.log(canvas.width)
-        console.log(canvas.height)
-    }
 
     glframe.appendChild(canvas)
     document.body.appendChild(glframe)
 
-    const url = "data/3dobjs/base.obj"
-    const scene = new WavefrontObj()
-
-//     const response = await fetch(url)
-//     if (response.status !== 200) {
-//         alert(`Failed to read ${url}: ${response.status} ${response.statusText}`)
-//         return
-//     }
-//     // const data = await response.arrayBuffer()
-//     // const reader = response.body?.getReader()!!
-//     let data = ""
-//     const stream = response.body?.getReader()!!
-//     const reader = readline.createInterface(stream)
-// -   for (const line of reader) {
-//         data += line
-//     }
-
-    // const x = (await reader?.read()).value
-    // const data = new TextDecoder("utf-8").decode(x)
-    // console.log(x)
-
-    // console.log(r)
-    // .then(response => response.json())
-    // .then(data2 => console.log(data2));
-
-    const data = await get(url)
-    scene.load(data)
-
-    // const canvas = document.querySelector('#glcanvas') as HTMLCanvasElement | null
     if (canvas === null)
         throw Error("No #glcanvas")
     const gl = (canvas.getContext('webgl2') || canvas.getContext('experimental-webgl')) as WebGL2RenderingContext
@@ -137,14 +95,8 @@ async function main() {
       gl_FragColor = vec4(vec3(1,0.8,0.7) * vLighting, 1.0);
     }`
 
-    // Initialize a shader program; this is where all the lighting
-    // for the vertices and so forth is established.
-    const shaderProgram = initShaderProgram(gl, vertextShaderProgram, fragmentShaderProgram)
+    const shaderProgram = compileShaders(gl, vertextShaderProgram, fragmentShaderProgram)
 
-    // Collect all the info needed to use the shader program.
-    // Look up which attributes our shader program is using
-    // for aVertexPosition, aVevrtexColor and also
-    // look up uniform locations.
     const programInfo = {
         program: shaderProgram,
         attribLocations: {
@@ -159,12 +111,12 @@ async function main() {
         }
     }
 
-    // Here's where we call the routine that builds all the objects we'll be drawing.
-    const buffers = initBuffers(gl, scene)
+    const url = "data/3dobjs/base.obj"
+    const scene = new WavefrontObj()
+    scene.load(await get(url))
+    const buffers = createAllBuffers(gl, scene)
 
     let then = 0
-
-    // Draw the scene repeatedly
     function render(now: number) {
         now *= 0.001;  // convert to seconds
         const deltaTime = now - then
@@ -177,37 +129,27 @@ async function main() {
     requestAnimationFrame(render)
 }
 
-//
-// initBuffers
-//
-// Initialize the buffers we'll need. For this demo, we just
-// have one object -- a simple three-dimensional cube.
-//
-function initBuffers(gl: WebGL2RenderingContext, scene: WavefrontObj) {
-    const vertex = new Float32Array(scene.vertex)
-    const indices = new Uint16Array(scene.indices)
+interface Buffers {
+    position: WebGLBuffer
+    normal: WebGLBuffer
+    indices: WebGLBuffer
+}
 
-    // POSITIONS
-    const vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, vertex, gl.STATIC_DRAW)
-
-    // NORMALS
-    const normalBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer)
-    const normals = calculateNormals(scene)
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW)
-
-    const indexBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
-
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
-
+function createAllBuffers(gl: WebGL2RenderingContext, scene: WavefrontObj): Buffers {
     return {
-        position: vertexBuffer,
-        normal: normalBuffer,
-        indices: indexBuffer,
+        position: createBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW, Float32Array, scene.vertex),
+        normal: createBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW, Float32Array, calculateNormals(scene)),
+        indices: createBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, gl.STATIC_DRAW, Uint16Array, scene.indices)
     }
+}
+
+function createBuffer(gl: WebGL2RenderingContext, target: GLenum, usage: GLenum, type: any, data: number[]): WebGLBuffer {
+    const buffer = gl.createBuffer()
+    if (buffer === null)
+        throw Error("Failed to create new WebGLBuffer")
+    gl.bindBuffer(target, buffer)
+    gl.bufferData(target, new type(data), usage)
+    return buffer
 }
 
 function addNormal(normals: number[], counter: number[], index: number, normal: vec3) {
@@ -261,21 +203,6 @@ function calculateNormals(scene: WavefrontObj) {
     return normals
 }
 
-// function createProjectionMatrix(fieldOfViewInRadians: number, aspectRatio: number, near: number, far: number) {
-//     const f = 1.0 / Math.tan(fieldOfViewInRadians / 2);
-//     const rangeInv = 1 / (near - far);
-
-//     return new DOMMatrix([
-//         f / aspectRatio, 0, 0, 0,
-//         0, f, 0, 0,
-//         0, 0, (near + far) * rangeInv, -1,
-//         0, 0, near * far * rangeInv * 2, 0
-//     ])
-// }
-
-//
-// Draw the scene.
-//
 function drawScene(gl: WebGL2RenderingContext, programInfo: any, buffers: any, deltaTime: number, scene: WavefrontObj) {
     const canvas = gl.canvas as HTMLCanvasElement
     if (canvas.width !== canvas.clientWidth || canvas.height !== canvas.clientHeight) {
@@ -289,30 +216,15 @@ function drawScene(gl: WebGL2RenderingContext, programInfo: any, buffers: any, d
     gl.enable(gl.DEPTH_TEST)            // Enable depth testing
     gl.depthFunc(gl.LEQUAL)             // Near things obscure far things
 
-    // Clear the canvas before we start drawing on it.
-
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
-
     const fieldOfView = 45 * Math.PI / 180    // in radians
-    // const canvas = gl.canvas as HTMLCanvasElement
     const aspect = canvas.clientWidth / canvas.clientHeight
     const zNear = 0.1
     const zFar = 100.0
     const projectionMatrix = mat4.create()
-    // const projectionMatrix = createProjectionMatrix(fieldOfView, aspect, zNear, zFar)
-
-    // note: glmatrix.js always has the first argument
-    // as the destination to receive the result.
     mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar)
 
-    // Set the drawing position to the "identity" point, which is the center of the scene.
     const modelViewMatrix = mat4.create()
     mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -25.0]) // move the model (cube) away
     mat4.rotate(modelViewMatrix,  modelViewMatrix,  cubeRotation, [0, 0, 1])
@@ -322,8 +234,6 @@ function drawScene(gl: WebGL2RenderingContext, programInfo: any, buffers: any, d
     mat4.invert(normalMatrix, modelViewMatrix);
     mat4.transpose(normalMatrix, normalMatrix);
 
-    // Tell WebGL how to pull out the positions from the position
-    // buffer into the vertexPosition attribute
     {
         const numComponents = 3
         const type = gl.FLOAT
@@ -342,8 +252,6 @@ function drawScene(gl: WebGL2RenderingContext, programInfo: any, buffers: any, d
             programInfo.attribLocations.vertexPosition)
     }
 
-    // Tell WebGL how to pull out the normals from
-    // the normal buffer into the vertexNormal attribute.
     {
         const numComponents = 3
         const type = gl.FLOAT
@@ -361,34 +269,10 @@ function drawScene(gl: WebGL2RenderingContext, programInfo: any, buffers: any, d
         gl.enableVertexAttribArray(
             programInfo.attribLocations.vertexNormal)
     }
-
-    // Tell WebGL how to pull out the colors from the color buffer
-    // into the vertexColor attribute.
-    // {
-    //     const numComponents = 4; // RGBA
-    //     const type = gl.FLOAT;
-    //     const normalize = false;
-    //     const stride = 0;
-    //     const offset = 0;
-    //     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color)
-    //     gl.vertexAttribPointer(
-    //         programInfo.attribLocations.vertexColor,
-    //         numComponents,
-    //         type,
-    //         normalize,
-    //         stride,
-    //         offset);
-    //     gl.enableVertexAttribArray(
-    //         programInfo.attribLocations.vertexColor)
-    // }
-
-    // Tell WebGL which indices to use to index the vertices
+ 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices)
-    // Tell WebGL to use our program when drawing
 
     gl.useProgram(programInfo.program)
-
-    // Set the shader uniforms
 
     gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix)
     gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix,  false, modelViewMatrix)
@@ -401,45 +285,42 @@ function drawScene(gl: WebGL2RenderingContext, programInfo: any, buffers: any, d
         gl.drawElements(gl.TRIANGLES, indexCount, type, offset)
     }
 
-    // Update the rotation for the next draw
-
     cubeRotation += deltaTime
 }
 
 //
 // Initialize a shader program, so WebGL knows how to draw our data
 //
-function initShaderProgram(gl: any, vsSource: string, fsSource: string) {
-    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)
-    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)
+function compileShaders(gl: any, vertexSharderSrc: string, fragmentShaderSrc: string) {
+    const vertexShader = compileShader(gl, gl.VERTEX_SHADER, vertexSharderSrc)
+    const fragmentShader = compileShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSrc)
 
-    // Create the shader program
-    const shaderProgram = gl.createProgram()
-    gl.attachShader(shaderProgram, vertexShader)
-    gl.attachShader(shaderProgram, fragmentShader)
-    gl.linkProgram(shaderProgram)
+    const program = gl.createProgram()
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader)
+    gl.linkProgram(program)
 
     // If creating the shader program failed, alert
 
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram))
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program))
         return null
     }
 
-    return shaderProgram
+    return program
 }
 
 //
 // creates a shader of the given type, uploads the source and compiles it.
 //
-function loadShader(gl: WebGL2RenderingContext, type: GLenum, source: string) {
+function compileShader(gl: WebGL2RenderingContext, type: GLenum, source: string) {
     const shader = gl.createShader(type)
     if (shader === null)
         throw Error("failed to create shader")
     gl.shaderSource(shader, source)
     gl.compileShader(shader)
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader))
+        alert(`An error occurred compiling the ${type} shader: ${gl.getShaderInfoLog(shader)}`)
         gl.deleteShader(shader)
         return null
     }
