4709c9a707426f2c01e01200e294dbd487362755
Mark-André Hopf <mhopf@mark13.org>
2020-09-20 11:19:04 +0200

    add missing files

---
+++ src/fileformats/StringToLine.ts
@@ -0,0 +1,12 @@
+import { StringToLineIterator } from "./StringToLineIterator"
+
+export class StringToLine implements Iterable<string> {
+    data: string;
+    constructor(data: string) {
+        this.data = data
+    }
+
+    [Symbol.iterator](): Iterator<string> {
+        return new StringToLineIterator(this.data)
+    }
+}
---
+++ src/fileformats/StringToLineIterator.ts
@@ -0,0 +1,30 @@
+
+export class StringToLineIterator implements Iterator<string> {
+    private data: string
+    private index: number
+
+    constructor(data: string) {
+        this.data = data
+        this.index = 0
+    }
+
+    next(): IteratorResult<string> {
+        if (this.data.length === 0 || this.index === -1)
+            return { value: undefined, done: true }
+        const nextIndex = this.data.indexOf("\n", this.index)
+        let length: number | undefined
+        if (nextIndex === -1) {
+            length = undefined
+        } else {
+            length = nextIndex - this.index
+        }
+        const result = this.data.substr(this.index, length)
+        if (nextIndex === -1) {
+            this.index = -1
+        } else {
+            this.index = nextIndex + 1
+        }
+        return { value: result, done: false }
+    }
+
+}
