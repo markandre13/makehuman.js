8a57ef6781c9202844415644e396d033640450f0
Mark-André Hopf <mhopf@mark13.org>
2023-01-22 21:23:30 +0100

    use rollup's terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
 export default {
     input: './src/main.tsx',
@@ -23,6 +24,7 @@ export default {
             sourceMap: true
         }),
         nodeResolve(),
-        commonjs()
+        commonjs(),
+        terser()
     ]
 }
