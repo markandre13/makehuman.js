19feeb7ae30dd1d0c1760cdd232ccaf9f72ef2c9
Mark-André Hopf <mhopf@mark13.org>
2023-02-05 17:43:07 +0100

    collada: import into blender now shows something :)

--- src/mesh/Collada.ts
+++ src/mesh/Collada.ts
@@ -9,6 +9,43 @@ import { calculateNormals } from '../../src/lib/calculateNormals'
 
 export function exportCollada(scene: HumanMesh) {
 
+    // todo:
+    // [ ] export just the skin vertices
+    // [ ] export normals
+
+    // a test cube
+
+    // const scene = {
+    //     vertex: [
+    //          1,  1,  1,
+    //          1,  1, -1,
+    //          1, -1,  1,
+    //          1, -1, -1,
+    //         -1,  1,  1,
+    //         -1,  1, -1,
+    //         -1, -1,  1,
+    //         -1, -1, -1 
+    //     ],
+    //     indices: [
+    //         4, 2, 0,  
+    //         2, 7, 3,   
+    //         6, 5, 7,  
+    //         1, 7, 5,   
+    //         0, 3, 1,  
+    //         4, 1, 5,  
+    //         4, 6, 2,  
+    //         2, 6, 7,  
+    //         6, 4, 5,  
+    //         1, 3, 7,  
+    //         0, 2, 3,  
+    //         4, 0, 1  
+    //     ],
+    //     groups: [{
+    //         startIndex: 0,
+    //         length: 3 * 12
+    //     }]
+    // }
+
     let e = scene.groups[Mesh.SKIN].startIndex + scene.groups[0].length
     let polygons = " "
     for (let i = scene.groups[Mesh.SKIN].startIndex; i < e; ++i) {
@@ -17,55 +54,71 @@ export function exportCollada(scene: HumanMesh) {
 
     const normals = calculateNormals(scene.vertex, scene.indices)
     let header = `<?xml version="1.0" encoding="utf-8"?>
-<COLLADA version="1.4.1" xmlns="http://www.collada.org/2005/11/COLLADASchema">
+<COLLADA xmlns="http://www.collada.org/2005/11/COLLADASchema" version="1.4.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <asset>
     <contributor>
-      <author>https://github.com/markandre13/makehuman.js</author>
+      <author>makehuman.js user</author>
+      <authoring_tool>https://github.com/markandre13/makehuman.js</authoring_tool>
     </contributor>
     <created>${new Date().toISOString()}</created>
     <modified>${new Date().toISOString()}</modified>
-    <unit meter="0.1000" name="decimeter"/>
-    <up_axis>Y_UP</up_axis>
+    <unit name="meter" meter="1"/>
+    <up_axis>Z_UP</up_axis>
   </asset>
   <library_geometries>
-    <geometry id="skin" name="skin">
+    <geometry id="skin-mesh" name="skin">
       <mesh>
-        <source id="skin-position">
-          <float_array count="${scene.vertex.length}" id="skin-position-array">
+
+        <source id="skin-mesh-positions">
+          <float_array id="skin-mesh-positions-array" count="${scene.vertex.length}">
             ${scene.vertex.map(v => `${v}`).join(" ")}
           </float_array>
           <technique_common>
-            <accessor count="${scene.vertex.length / 3}" source="#skin-position-array" stride="3">
-              <param type="float" name="X"></param>
-              <param type="float" name="Y"></param>
-              <param type="float" name="Z"></param>
+            <accessor source="#skin-mesh-positions-array" count="${scene.vertex.length / 3}" stride="3">
+              <param name="X" type="float"/>
+              <param name="Y" type="float"/>
+              <param name="Z" type="float"/>
             </accessor>
           </technique_common>
         </source>
-        <source id="skin-normals">
-          <float_array count="${normals.length}" id="skin-normals-array">
+
+        <source id="skin-mesh-normals">
+          <float_array id="skin-mesh-normals-array" count="${normals.length}">
             ${normals.map(v => `${v}`).join(" ")}
           </float_array>
           <technique_common>
-            <accessor count="${normals.length / 3}" source="#skin-normals-array" stride="3">
-              <param type="float" name="X"></param>
-              <param type="float" name="Y"></param>
-              <param type="float" name="Z"></param>
+            <accessor source="#skin-mesh-normals-array" count="${normals.length / 3}" stride="3">
+              <param name="X" type="float"/>
+              <param name="Y" type="float"/>
+              <param name="Z" type="float"/>
             </accessor>
           </technique_common>
         </source>
-        <vertices id="skin-vertex">
-          <input semantic="POSITION" source="#skin-position"/>
+
+        <vertices id="skin-mesh-vertices">
+          <input semantic="POSITION" source="#skin-mesh-positions"/>
         </vertices>
-        <polylist count="${scene.groups[Mesh.SKIN].length}">
-          <input offset="0" semantic="VERTEX" source="#skin-vertex"/>
-          <input offset="1" semantic="NORMAL" source="#skin-normals"/>
-          <vcount>${`3 `.repeat(scene.groups[Mesh.SKIN].length)}</vcount>
+
+        <triangles count="${scene.groups[Mesh.SKIN].length}">
+          <input semantic="VERTEX" source="#skin-mesh-vertices" offset="0"/>
           <p>${polygons}</p>
-        </polylist>
-      </mesh>
+        </triangles>
+
+        </mesh>
     </geometry>
   </library_geometries>
+  <library_visual_scenes>
+    <visual_scene id="Scene" name="Scene">
+      <node id="skin" name="skin" type="NODE">
+        <matrix sid="transform">-1 0 0 0 0 0 1 0 0 1 0 0 0 0 0 1</matrix>
+        <instance_geometry url="#skin-mesh" name="skin">
+        </instance_geometry>
+      </node>
+    </visual_scene>
+  </library_visual_scenes>
+  <scene>
+    <instance_visual_scene url="#Scene"/>
+  </scene>
 </COLLADA>
 `
     return header
