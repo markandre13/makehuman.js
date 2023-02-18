b4a091639f3ebf2ee0cd1a1b781878130a27ac99
Mark-André Hopf <mhopf@mark13.org>
2023-02-18 18:44:03 +0100

    collada: extend tests without finding issues

--- test/mesh/Collada.spec.ts
+++ test/mesh/Collada.spec.ts
@@ -31,7 +31,6 @@ import { parseXML, Tag, Text } from "./xml"
 //   [ ] find out how/where some mesh vertices and bones are placed in my and the reference file
 
 /*
-
     okay, let's find out where 
       ~/Documents/Blender/objects/people/dariya/dariya.dae
     and what i currently export differ
@@ -39,7 +38,6 @@ import { parseXML, Tag, Text } from "./xml"
     * both have <up_axis>Z_UP</up_axis>
     * to compare <library_geometries> i'll use dariya-separate.dae where the mesh
       is not a child of the skeleton
-
 */
 
 describe("Collada", function () {
@@ -61,87 +59,140 @@ describe("Collada", function () {
         const collada = findTag(document, "COLLADA")
         expect(collada).to.not.be.undefined
 
+        // scene
+        //     instance_visual_scene.url=#{sceneId}
         const scene = findTag(collada, "scene")
         const instance_visual_scene = findTag(scene, "instance_visual_scene")
-        const sceneUrl = instance_visual_scene.attributes.get("url")
-        expect(sceneUrl).to.not.be.undefined
-        expect(sceneUrl?.charAt(0)).to.equal("#")
-        const sceneName = sceneUrl!.substring(1)
+        const sceneId = reference(instance_visual_scene.attributes.get("url"))
 
+        // library_visual_scenes
+        //     visual_scene id={sceneId}
+        //         node type=NODE
+        //             node type=JOINT id={skeletonId}
+        //                 ...more joints...
+        //             node type=NODE
+        //                 instance_controller.url=#{controllerId}
+        //                     skeleton -> #{skeletonId}
         const library_visual_scenes = findTag(collada, "library_visual_scenes")
         const visual_scene = findTag(library_visual_scenes, "visual_scene")
-        expect(visual_scene.attributes.get("id")).to.equal(sceneName)
+        expect(visual_scene.attributes.get("id")).to.equal(sceneId)
 
-        const node = findTag(visual_scene, "node")
-        expect(node.attributes.get("type")).to.equal("NODE")
-
-        const nodes = findTags(node, "node")
+        const rootNode = findTag(visual_scene, "node")
+        expect(rootNode.attributes.get("type")).to.equal("NODE")
+        const nodes = findTags(rootNode, "node")
         expect(nodes).to.have.lengthOf(2)
+
+        const controllerNode = nodes[1]
+        expect(controllerNode.attributes.get("type")).to.equal("NODE")
+        const instance_controller = findTag(controllerNode, "instance_controller")
+        const constollerId = reference(instance_controller.attributes.get("url"))
+        const skeleton = findTag(instance_controller, "skeleton")
+        const skeletonId = reference(text(skeleton))
+
         const skeletonNode = nodes[0]
-        const meshNode = nodes[1]
         expect(skeletonNode.attributes.get("type")).to.equal("JOINT")
-        expect(meshNode.attributes.get("type")).to.equal("NODE")
+        expect(skeletonNode.attributes.get("id")).to.equal(skeletonId)
 
-        const instance_controller = findTag(meshNode, "instance_controller") // url="#human_human_body-skin"
-        const skinUrl = instance_controller.attributes.get("url")
-        expect(skinUrl).to.not.be.undefined
-        expect(skinUrl?.charAt(0)).to.equal("#")
-        const skinControllerName = skinUrl!.substring(1)
-        const skeleton = findTag(instance_controller, "skeleton") // text: #human_Bone
-
+        // library_controllers
+        //     controller id={controllerId}
+        //         skin source=#${skinId}
+        //             source ; jointName to jointIndex
+        //             source ; jointIndex to InvBindMatrix
+        //             source ; jointIndex to weights
+        //             joints
+        //                 input ; jointIndex
+        //                 input ; invBindMatrixIndex
+        //             vertex_weights
+        //                 input ; jointIndex
+        //                 input ; weightIndex
+        //                 vcount
+        //                 v
         const library_controllers = findTag(collada, "library_controllers")
         const controller = findTag(library_controllers, "controller")
-        expect(controller.attributes.get("id")).to.equal(skinControllerName)
+        expect(controller.attributes.get("id")).to.equal(constollerId)
 
         const skin = findTag(controller, "skin")
-        const skinSource = skin.attributes.get("source")
-        expect(skinSource).to.not.be.undefined
-        expect(skinSource?.charAt(0)).to.equal("#")
-        const skinSourceName = skinSource!.substring(1) // skin-mesh
+        const skinId = reference(skin.attributes.get("source"))
 
+        const jointsNode = findTag(skin, "joints")
+        const jointInputs = findTags(jointsNode, "input")
+        expect(jointInputs).to.have.lengthOf(2)
+        expect(jointInputs[0].attributes.get("semantic")).to.equal("JOINT")
+        const jointsJointId = reference(jointInputs[0].attributes.get("source"))
+        expect(jointInputs[1].attributes.get("semantic")).to.equal("INV_BIND_MATRIX")
+        const jointsInvBindMatrixId = reference(jointInputs[1].attributes.get("source"))
+
+        const vertexWeights = findTag(skin, "vertex_weights")
+        const vertexWeightInputs = findTags(vertexWeights, "input")
+        expect(vertexWeightInputs).to.have.lengthOf(2)
+        expect(vertexWeightInputs[0].attributes.get("semantic")).to.equal("JOINT")
+        const vertexWeightsJointsId = reference(vertexWeightInputs[0].attributes.get("source"))
+        expect(vertexWeightInputs[1].attributes.get("semantic")).to.equal("WEIGHT")
+        const vertexWeightsWeightId = reference(vertexWeightInputs[1].attributes.get("source"))
+        // vcount
+        // v
+
+        const skinSources = findTags(skin, "source")
+        expect(skinSources).to.have.lengthOf(3)
+
+        const skinSourceJointNames = skinSources[0]
+        expect(skinSourceJointNames.attributes.get("id")).to.equal(jointsJointId)
+        expect(skinSourceJointNames.attributes.get("id")).to.equal(vertexWeightsJointsId)
+        const jointNames = findTag(skinSourceJointNames, "Name_array")
+
+        const skinSourcesBindPoses = skinSources[1]
+        expect(skinSourcesBindPoses.getAttribute("id")).to.equal(jointsInvBindMatrixId)
+        const invBindMatrix = findTag(skinSourcesBindPoses, "float_array")
+
+        const skinSourcesWeights = skinSources[2]
+        expect(skinSourcesWeights.getAttribute("id")).to.equal(vertexWeightsWeightId)
+        const weight = findTag(skinSourcesBindPoses, "float_array")
+
+        // library_geometries
+        //     geometry
+        //         mesh
+        //             source ; position
+        //             source ; normal
+        //             vertices
+        //             triangles
         const library_geometries = findTag(collada, "library_geometries")
         const geometry = findTag(library_geometries, "geometry")
-        expect(geometry.attributes.get("id")).to.equal(skinSourceName)
+        expect(geometry.attributes.get("id")).to.equal(skinId)
 
         const mesh = findTag(geometry, "mesh")
-        const meshSources = findTags(mesh, "source")
-        expect(meshSources).to.have.lengthOf(2)
-
-        const vertexSource = meshSources[0]
-        const normalSource = meshSources[1]
-
-        expect(vertexSource.attributes.get("id")).endsWith("-positions")
-        expect(normalSource.attributes.get("id")).endsWith("-normals")
-
-        const vertexValues = findTag(vertexSource, "float_array")
-        expect(text(vertexValues)).to.equal("1 1 1 1 1 -1 1 -1 1 1 -1 -1 -1 1 1 -1 1 -1 -1 -1 1 -1 -1 -1 1 1 0 -1 -1 0 1 -1 0 -1 1 0")
-
-        const vertices = findTag(mesh, "vertices")
-        const verticesInput = findTag(vertices, "input")
-        expect(verticesInput.attributes.get("semantic")).to.equal("POSITION")
-        const verticesInputSource = verticesInput.attributes.get("source")
-        expect(verticesInputSource).to.not.be.undefined
-        expect(verticesInputSource?.charAt(0)).to.equal("#")
-        const verticesInputSourceName = verticesInputSource!.substring(1)
-        expect(vertexSource.attributes.get("id")).to.equal(verticesInputSourceName)
 
         const triangles = findTag(mesh, "triangles") // FIXME: the makehuman mesh has lot's of quads which look better when subsurfacing
         expect(triangles.attributes.get("count")).to.equal("20")
         const triangleInput = findTags(triangles, "input")
         expect(triangleInput).to.have.lengthOf(2)
-        const vertexInput = triangleInput[0]
-        const normalInput = triangleInput[1]
-        expect(vertexInput.attributes.get("semantic")).to.equal("VERTEX")
-        expect(normalInput.attributes.get("semantic")).to.equal("NORMAL")
 
-        const vertexInputSource = vertexInput.attributes.get("source")
-        expect(vertexInputSource).to.not.be.undefined
-        expect(vertexInputSource?.charAt(0)).to.equal("#")
-        const vertexInputSourceName = vertexInputSource!.substring(1)
-        expect(vertices.attributes.get("id")).to.equal(vertexInputSourceName)
+        const vertexInput = triangleInput[0]
+        expect(vertexInput.attributes.get("semantic")).to.equal("VERTEX")
+        const triangleVertexSource = reference(vertexInput.getAttribute("source"))
+
+        const normalInput = triangleInput[1]
+        expect(normalInput.attributes.get("semantic")).to.equal("NORMAL")
+        const triangleNormalSource = reference(normalInput.getAttribute("source"))
 
         const triangleP = findTag(triangles, "p")
         expect(text(triangleP)).to.equal("4 2 0 2 9 10 6 11 9 1 7 5 0 10 8 4 8 11 11 1 5 8 3 1 9 5 7 10 7 3 4 6 2 2 6 9 6 4 11 1 3 7 0 2 10 4 0 8 11 8 1 8 10 3 9 11 5 10 9 7")
+
+        const vertices = findTag(mesh, "vertices")
+        expect(vertices.getAttribute("id")).to.equal(triangleVertexSource)
+        const verticesInput = findTag(vertices, "input")
+        expect(verticesInput.attributes.get("semantic")).to.equal("POSITION")
+        const verticesPositionSource = reference(verticesInput.attributes.get("source"))
+
+        const meshSources = findTags(mesh, "source")
+        expect(meshSources).to.have.lengthOf(2)
+
+        const vertexPositionSource = meshSources[0]
+        expect(vertexPositionSource.attributes.get("id")).to.equal(verticesPositionSource)
+        const vertexPositionValues = findTag(vertexPositionSource, "float_array")
+        expect(text(vertexPositionValues)).to.equal("1 1 1 1 1 -1 1 -1 1 1 -1 -1 -1 1 1 -1 1 -1 -1 -1 1 -1 -1 -1 1 1 0 -1 -1 0 1 -1 0 -1 1 0")
+
+        const normalSource = meshSources[1]
+        expect(normalSource.attributes.get("id")).to.equal(triangleNormalSource)
 
         // console.log(xml)
     })
@@ -227,4 +278,14 @@ function text(tag: Tag): string {
         throw Error()
     }
     return (tag.children[0] as Text).text.trim()
+}
+
+function reference(ref: string | undefined): string {
+    if (ref === undefined) {
+        throw Error(`reference is not defined`)
+    }
+    if (ref.length === 0 || ref.charAt(0) !== '#') {
+        throw Error(`'${ref}' must begin with '#'`)
+    }
+    return ref.substring(1)
 }
