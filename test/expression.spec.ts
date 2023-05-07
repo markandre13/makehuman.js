7891e51d37b5d64d283b6678f2d491df8594a2b2
Mark-André Hopf <mhopf@mark13.org>
2023-05-07 10:55:19 +0200

    expression: getBlendedPose()

--- test/expression.spec.ts
+++ test/expression.spec.ts
@@ -4,7 +4,7 @@ use(chaiString)
 import { chaiAlmost } from "./chai/chaiAlmost"
 use(chaiAlmost(0.00001))
 
-import { mat4, vec3 } from 'gl-matrix'
+import { mat4, vec3, quat, quat2 } from 'gl-matrix'
 
 import { FileSystemAdapter } from '../src/filesystem/FileSystemAdapter'
 import { HTTPFSAdapter } from '../src/filesystem/HTTPFSAdapter'
@@ -33,6 +33,13 @@ describe("expression", function () {
  ${m[4]}, ${m[5]}, ${m[6]}, ${m[7]},
  ${m[8]}, ${m[9]}, ${m[10]}, ${m[11]},
  ${m[12]}, ${m[13]}, ${m[14]}, ${m[15]}]`
+    }
+
+    function q2s(name: string, q: quat) {
+        return `${name} = [${q[0]}, ${q[1]}, ${q[2]}, ${q[3]}]`
+    }
+    function q22s(name: string, q: quat2) {
+        return `${name} = [${q[0]}, ${q[1]}, ${q[2]}, ${q[3]}, ${q[4]}, ${q[5]}, ${q[6]}, ${q[7]}]`
     }
 
     it("python matrix order", function () {
@@ -260,7 +267,8 @@ describe("expression", function () {
         })
     })
 
-    it.only("createAnimationTrack()", function () {
+    it("createAnimationTrack()", function () {
+        // GIVEN some real world MH data
         const mgr = new ExpressionManager()
 
         const human = new Human()
@@ -271,8 +279,10 @@ describe("expression", function () {
         const boneCount = skeleton.boneslist!.length
         const frameCount = mgr.facePoseUnits.frameCount
 
+        // WHEN createAnimationTrack()
         const base_anim: mat4[] = mgr.facePoseUnits.createAnimationTrack(skeleton, "Expression-Face-PoseUnits")
 
+        // THEN it's the same as in MH 1.2 (Python)
         let pythonIdx = 0, typescriptIdx = 0
         for (let frame = 0; frame < frameCount; ++frame) {
             for (let bone = 0; bone < boneCount; ++bone) {
@@ -280,73 +290,236 @@ describe("expression", function () {
                 for (let j = 0; j < 12; ++j) {
                     e[j] = base_anim_data[pythonIdx++]
                 }
-                mat4.transpose(e,e)
+                mat4.transpose(e, e)
                 const v = base_anim[typescriptIdx++]
                 expect(v, `bone ${bone} '${skeleton.boneslist![bone].name}', frame ${frame}`).to.deep.almost.equal(e)
             }
         }
+    })
 
-        // base_anim_data:
-        // frame -> rig -> matrix
+    /**
+     * Return spherical linear interpolation between two quaternions.
+     * 
+     * @param quat0 
+     * @param quat1 
+     * @param fraction 
+     * @param spin 
+     * @param shortestpath 
+     */
+    function quaternion_slerp(quat0: quat2, quat1: quat2, fraction: number, spin = 0, shortestpath = true) {
+        const _EPS = Number.EPSILON * 4.0
 
-        // console.log(`base_anim_data.length = ${base_anim_data.length/skeleton.boneslist!.length/12}`)
+        let q0 = quat2.copy(quat2.create(), quat0)
+        const q1 = quat2.copy(quat2.create(), quat1)
 
-        // for(let frame = 0; frame<mgr.facePoseUnits.frameCount; ++frame) {
-        // for (let b_idx = 0, pmIdx = 0; b_idx < skeleton.boneslist!.length; ++b_idx) {
-        //     const expectPoseMat = mat4.create()
-        //     for (let j = 0; j < 12; ++j) {
-        //         expectPoseMat[j] = base_anim_data[pmIdx++]
-        //     }
-        //     mat4.transpose(expectPoseMat, expectPoseMat)
+        // console.log(`Q0 = ${quat0}`)
+        // console.log(`Q0 = ${q0}`)
+        // console.log(`Q1 = ${quat1}`)
+        // console.log(`Q1 = ${q1}`)
 
-        //     expect(base_anim[b_idx], m2s(`base_anim[${b_idx}]`, base_anim[b_idx]) + m2s(`\nexpectPoseMat`, expectPoseMat)).to.deep.almost.equal(expectPoseMat)
-        //     // console.log(m2s(`base_anim[${b_idx}]`, base_anim[b_idx]))
-        //     }
-        // }
+        if (fraction === 0.0) {
+            return q0
+        }
+        if (fraction === 1.0) {
+            return q1
+        }
+        let d = quat2.dot(q0 as any, q1 as any)
+
+        if (Math.abs(Math.abs(d) - 1.0) < _EPS) {
+            return q0
+        }
+        if (shortestpath && d < 0.0) {
+            // invert rotation
+            d = -d
+            for (let i = 0; i < q0.length; ++i) {
+                q0[i] = -q0[i]
+            }
+        }
+        const angle = Math.acos(d) + spin * Math.PI
+        if (Math.abs(angle) < _EPS) {
+            return q0
+        }
+        const isin = 1.0 / Math.sin(angle)
+        const a0 = Math.sin((1.0 - fraction) * angle) * isin
+        const a1 = Math.sin(fraction * angle) * isin
+        const q2 = quat2.create()
+        for (let i = 0; i < q0.length; ++i) {
+            q0[i] *= a0
+            q1[i] *= a1
+            q2[i] = q1[i] + q0[i]
+        }
+        return q2
+    }
+
+    describe("quaternions", function () {
+        xit("identity", function () {
+            const identity = quat.create()      // [0, 0, 0, 1]
+            console.log(q2s("identity", identity))
+            const identity2 = quat2.create()    // [0, 0, 0, 1, 0, 0, 0, 0]
+            console.log(q22s("identity2", identity2))
+        })
+        it("slerp(quat2, quat2, number): quat2", function () {
+            const q0 = quat2.fromMat4(quat2.create(), mat4.fromXRotation(mat4.create(), 0.1))
+            const q1 = quat2.fromMat4(quat2.create(), mat4.fromYRotation(mat4.create(), 0.11))
+            const s = quaternion_slerp(q0, q1, 0.3)
+            expect(s).to.deep.almost.equal(new Float32Array([0.03500185, 0.0165055, 0, 0.99925094, 0, 0, 0, 0]))
+
+            // const q2 = quat2.fromMat4(quat2.create(), mat4.fromZRotation(mat4.create(), 0.13))
+
+            // console.log(m2s("t", mat4.fromTranslation(mat4.create(), [1, 2, 3])))
+            // console.log(m2s("rX 0.1", mat4.fromXRotation(mat4.create(), 0.1)))
+            // console.log(m2s("rY 0.1", mat4.fromYRotation(mat4.create(), 0.1)))
+            // console.log(m2s("rZ 0.1", mat4.fromZRotation(mat4.create(), 0.1)))
+            // console.log(q22s("identity", quat2.create()))
+
+
+            // console.log(q22s("rZ 0.1", q2))
+            // const qm = quat2.multiply(quat2.create(), q1, q0)
+            // console.log(q22s("q1 * q0", qm))
+            // console.log(m2s("m", mat4.fromQuat2(mat4.create(), qm)))
+            // console.log(q2s("q1 * q0", quat.slerp(
+            //     quat.create(),
+            //     quat.fromValues(q1[0], q1[1], q1[2], q1[3]),
+            //     quat.fromValues(q1[0], q1[1], q1[2], q1[3]),
+            //     0.3))
+            // )
+
+            // console.log(q22s("slerp(q0, q1, 0.3)", quaternion_slerp(q0, q1, 0.3)))
+        })
+    })
+
+    it.only("poseFromUnitPose()", function () {
+        const mgr = new ExpressionManager()
+
+        const human = new Human()
+        const obj = new WavefrontObj('data/3dobjs/base.obj')
+        const scene = new HumanMesh(human, obj)
+        const skeleton = loadSkeleton(scene, "data/rigs/default.mhskel")
+        const base_anim: mat4[] = mgr.facePoseUnits.createAnimationTrack(skeleton, "Expression-Face-PoseUnits")
 
         // const name = "boring01"
-
         // const map = mgr.calculateExpression(
         //     mgr.expressions.findIndex((v) => v === name)
         // )
         // map.forEach( (value, key) => console.log(`${key} = ${value}`))
 
+        const name = "laugh01"
+        const unit_poses = JSON.parse(FileSystemAdapter.getInstance().readFile(`data/expressions/${name}.mhpose`))
+            .unit_poses as any
 
+        // Pose(AnimationTrack): fromPoseUnit(...)
+        const poses: number[] = []
+        const weights: number[] = []
+        for (let poseName of Object.getOwnPropertyNames(unit_poses)) {
+            poses.push(mgr.poseUnitName2Frame.get(poseName)!)
+            weights.push(unit_poses[poseName])
+        }
+        // console.log(weights)
+        // console.log(`poses`)
 
-        // const name = "laugh01"
-        // const unit_poses = JSON.parse(FileSystemAdapter.getInstance().readFile(`data/expressions/${name}.mhpose`))
-        //     .unit_poses as any
+        // PoseUnit(AnimationTrack): getBlendedPose(self, poses, weights, additiveBlending=True, only_data=False):
+        const additiveBlending = true
+        const onlyDate = true
+        const f_idxs = poses
+        const nBones = skeleton.boneslist!.length
 
-        // // poses & weight as in getBlendedPose(...)
-        // const poses: number[] = []
-        // const weight: number[] = []
-        // for (let poseName of Object.getOwnPropertyNames(unit_poses)) {
-        //     poses.push(mgr.poseUnitName2Frame.get(poseName)!)
-        //     weight.push(unit_poses[poseName])
-        // }
-        // // console.log(weight)
-        // // console.log(poses)
+        if (!additiveBlending) {
+            throw Error(`yikes`)
+        }
 
-        // 2_posing_expression.py
-        //     class ExpressionTaskView(gui3d.TaskView, filecache.MetadataCacher):
-        //         def _load_pose_units(self):
-        //              self.base_bvh = bvh.load("data/poseunits/face-poseunits.bvh", allowTranslation="none"))
+        const result: mat4[] = new Array(nBones)
 
-        //              TODO: createAnimationTrack() is not implemented yet
-        //              self.base_anim = self.base_bvh.createAnimationTrack(self.human.getBaseSkeleton(), name="Expression-Face-PoseUnits")
+        if (f_idxs.length === 1) {
+            throw Error(`yikes`)
+        } else {
 
-        //              self.poseunit_names = loadJson("poseunits/face-poseunits.json").framemapping
-        //              self.base_anim = animation.PoseUnit(self.base_anim.name, self.base_anim._data, self.poseunit_names)
-        //         def chooseExpression(self, filename):
-        //             self.selectedPose = animation.poseFromUnitPose('expr-lib-pose', filename, self.base_anim)
-        // animation.py
-        //     def poseFromUnitPose(name, filename, poseUnit):
-        //         return Pose(name, emptyPose()).fromPoseUnit(filename, poseUnit)
-        //     class Pose(AnimationTrack):
-        //         def fromPoseUnit(self, filename, poseUnit):
-        //             self._data = poseUnit.getBlendedPose(list(self.unitposes.keys()), list(self.unitposes.values()), only_data=True)
-        //     class PoseUnit(AnimationTrack):
-        //         def getBlendedPose(self, poses, weights, additiveBlending=True, only_data=False):
-        //             ...
+            const REST_QUAT = quat2.create()
+            const debug_bone = 63
+
+            for (let b_idx = 0; b_idx < nBones; ++b_idx) {
+                const m1 = base_anim[f_idxs[0] * nBones + b_idx]
+                const m2 = base_anim[f_idxs[1] * nBones + b_idx]
+
+                let q1 = quat2.fromMat4(quat2.create(), m1)
+                let q2 = quat2.fromMat4(quat2.create(), m2)
+
+                q1 = quaternion_slerp(REST_QUAT, q1, weights[0])
+                q2 = quaternion_slerp(REST_QUAT, q2, weights[1])
+
+                let quat = quat2.multiply(quat2.create(), q2, q1)
+
+                if (b_idx === debug_bone) {
+                    console.log(`b_idx = ${b_idx}`)
+                    console.log(q22s("q1", q1))
+                    console.log(q22s("q2", q2))
+                }
+
+                for (let i = 2; i < f_idxs.length; ++i) {
+                    const m = base_anim[f_idxs[i] * nBones + b_idx]
+                    let q = quat2.fromMat4(quat2.create(), m)
+                    q = quaternion_slerp(REST_QUAT, q, weights[i])
+                    quat = quat2.multiply(quat2.create(), q, quat)
+                    if (b_idx === debug_bone) {
+                        console.log(q22s(`q${i+1}`, q))
+                    }
+                }
+                result[b_idx] = mat4.fromQuat2(mat4.create(), quat)
+                if (b_idx === debug_bone) {
+                    console.log(q22s(`quat`, quat))
+                    console.log(m2s(`result[${b_idx}]`, result[b_idx]))
+                }
+            }
+        }
+
+        // CHECK THE RESULT
+        for (let frame = 0; frame < mgr.facePoseUnits.frameCount; ++frame) {
+            for (let b_idx = 0, pmIdx = 0; b_idx < skeleton.boneslist!.length; ++b_idx) {
+                const expectPoseMat = mat4.create()
+                for (let j = 0; j < 12; ++j) {
+                    expectPoseMat[j] = laugh01_OUT[pmIdx++]
+                }
+                mat4.transpose(expectPoseMat, expectPoseMat)
+
+                expect(result[b_idx], m2s(`result[${b_idx}]`, result[b_idx]) + m2s(`\nexpectPoseMat`, expectPoseMat) + `\n`).to.deep.almost.equal(expectPoseMat)
+            }
+        }
     })
+
+    // base_anim_data:
+    // frame -> rig -> matrix
+
+    // console.log(`base_anim_data.length = ${base_anim_data.length/skeleton.boneslist!.length/12}`)
+
+    // for(let frame = 0; frame<mgr.facePoseUnits.frameCount; ++frame) {
+    // for (let b_idx = 0, pmIdx = 0; b_idx < skeleton.boneslist!.length; ++b_idx) {
+    //     const expectPoseMat = mat4.create()
+    //     for (let j = 0; j < 12; ++j) {
+    //         expectPoseMat[j] = base_anim_data[pmIdx++]
+    //     }
+    //     mat4.transpose(expectPoseMat, expectPoseMat)
+
+    //     expect(base_anim[b_idx], m2s(`base_anim[${b_idx}]`, base_anim[b_idx]) + m2s(`\nexpectPoseMat`, expectPoseMat)).to.deep.almost.equal(expectPoseMat)
+    //     // console.log(m2s(`base_anim[${b_idx}]`, base_anim[b_idx]))
+    //     }
+    // }
+
+    // 2_posing_expression.py
+    //     class ExpressionTaskView(gui3d.TaskView, filecache.MetadataCacher):
+    //         def _load_pose_units(self):
+    //              self.base_bvh = bvh.load("data/poseunits/face-poseunits.bvh", allowTranslation="none"))
+    //              self.base_anim = self.base_bvh.createAnimationTrack(self.human.getBaseSkeleton(), name="Expression-Face-PoseUnits")
+    //              self.poseunit_names = loadJson("poseunits/face-poseunits.json").framemapping
+    //              self.base_anim = animation.PoseUnit(self.base_anim.name, self.base_anim._data, self.poseunit_names)
+    //         def chooseExpression(self, filename):
+    //             self.selectedPose = animation.poseFromUnitPose('expr-lib-pose', filename, self.base_anim)
+    // animation.py
+    //     def poseFromUnitPose(name, filename, poseUnit):
+    //         return Pose(name, emptyPose()).fromPoseUnit(filename, poseUnit)
+    //     class Pose(AnimationTrack):
+    //         def fromPoseUnit(self, filename, poseUnit):
+    //             self._data = poseUnit.getBlendedPose(list(self.unitposes.keys()), list(self.unitposes.values()), only_data=True)
+    //     class PoseUnit(AnimationTrack):
+    //         def getBlendedPose(self, poses, weights, additiveBlending=True, only_data=False):
+    //             ...
+
 })
