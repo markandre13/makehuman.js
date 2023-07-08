0a6f576209308a27dbb16305b6854b8d7b2460a6
Mark-André Hopf <mhopf@mark13.org>
2023-07-08 10:53:40 +0200

    chordata: add Start/Stop buttons

--- src/ui/chordata.tsx
+++ src/ui/chordata.tsx
@@ -1,9 +1,23 @@
 import { TAB } from 'HistoryManager'
-import { Button } from "toad.js/view/Button"
+import { Action } from 'toad.js'
+import { Button, ButtonVariant } from "toad.js/view/Button"
 import { Tab } from "toad.js/view/Tab"
+
+const start = new Action(() => {
+    console.log("START")
+    start.enabled = false
+    stop.enabled = true
+})
+const stop = new Action(() => {
+    console.log("STOP")
+    start.enabled = true
+    stop.enabled = false
+})
+stop.enabled = false
 
 export default (
     <Tab label="Chordata" value={TAB.CHORDATA}>
-        <Button>Start</Button>
+        <Button variant={ButtonVariant.ACCENT} action={start}>Start</Button>
+        <Button variant={ButtonVariant.NEGATIVE} action={stop}>Stop</Button>
     </Tab>
 )
