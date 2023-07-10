3f8cac259831d516da4eccc1d274aa9e0ad08ec0
Mark-André Hopf <mhopf@mark13.org>
2023-07-10 11:42:40 +0200

    chordata: display text relative to bone

--- src/render/renderChordata.ts
+++ src/render/renderChordata.ts
@@ -1,4 +1,4 @@
-import { mat4, quat } from "gl-matrix"
+import { mat4, quat, vec4 } from "gl-matrix"
 import { RenderMesh } from "./RenderMesh"
 import { createNormalMatrix, createProjectionMatrix, prepareCanvas, prepareViewport } from "./render"
 import { RGBAShader } from "./shader/RGBAShader"
@@ -30,13 +30,6 @@ export function drawChordata(
     overlay: HTMLElement
 ) {
     initCone(gl)
-    if (overlay.children.length === 0) {
-        const s = span(text("Hello"))
-        s.style.position = "absolute"
-        s.style.top = "320px"
-        s.style.left = "360px"
-        overlay.appendChild(s)
-    }
 
     const canvas = gl.canvas as HTMLCanvasElement
     prepareCanvas(canvas)
@@ -44,12 +37,41 @@ export function drawChordata(
     const projectionMatrix = createProjectionMatrix(canvas)
 
     const modelViewMatrix = mat4.create()
-    mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -25.0]) // move the model away
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-10.0, -5.0, -25.0]) // move the model away
 
     const bone = bones.get("/%/kc_0x42branch6")!
     const q = quat.fromValues(bone[0], bone[1], bone[2], bone[3])
     const m = mat4.fromQuat(mat4.create(), q)
     mat4.multiply(modelViewMatrix, modelViewMatrix, m)
+
+    if (overlay.children.length === 0) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/WebGL_model_view_projection
+
+        // model -> MODEL MATRIX -> model in world -> PROJECTION MATRIX
+
+        // CLIPSPACE (-1,-1,-1) to (1,1,1)
+        // These composed matrices ultimately move the original model data around into a special
+        // coordinate space called clip space. This is a 2 unit wide cube, centered at (0,0,0), 
+        // and with corners that range from (-1,-1,-1) to (1,1,1). This clip space is compressed 
+        // down into a 2D space and rasterized into an image.
+
+        const m = mat4.multiply(mat4.create(), projectionMatrix, modelViewMatrix)
+
+        const point = vec4.fromValues(0, 0, 2, 1) // this is the front top right corner
+        const clipspace = vec4.transformMat4(vec4.create(), point, m)
+        clipspace[0] /= clipspace[3]
+        clipspace[1] /= clipspace[3]
+        const pixelX = (clipspace[0] *  0.5 + 0.5) * gl.canvas.width;
+        const pixelY = (clipspace[1] * -0.5 + 0.5) * gl.canvas.height;
+
+        const s = span(text("/%/kc_0x42branch6"))
+        s.style.position = "absolute"
+        s.style.left = `${pixelX}px`
+        s.style.top = `${pixelY}px`
+        overlay.replaceChildren(s)
+
+        console.log(`place text at (${pixelX},${pixelY})`)
+    }
 
     // mat4.rotate(modelViewMatrix, modelViewMatrix, 0.7, [0, 1, 0])
     const normalMatrix = createNormalMatrix(modelViewMatrix)
